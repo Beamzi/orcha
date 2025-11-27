42913200cf659821b7f10b37fbd891fedd8dd8aa
James Day <159420270+Beamzi@users.noreply.github.com>
2025-11-27 12:14:41 +1000

    asd

--- src/components/TestRenderPrompt.tsx
+++ src/components/TestRenderPrompt.tsx
@@ -3,6 +3,13 @@ import React, { useEffect, useState } from "react";
 
 export default function TestRenderPrompt() {
   const [responseContent, setResponseContent] = useState<string>("");
+  const [userPrompt, setUserPrompt] = useState("");
+  const [searchResults, setSearchResults] = useState();
+
+  async function getSearch() {
+    const request = await fetch("/api/get-search");
+    const response = await request.json();
+  }
 
   async function getPrompt() {
     let response = await fetch("http://localhost:11434/api/generate", {
@@ -10,7 +17,7 @@ export default function TestRenderPrompt() {
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
         model: "gemma3:1b",
-        prompt: "is the sky blue?",
+        prompt: `please condense this quoted prompt into an efficient websearch '${userPrompt}'`,
         stream: false,
       }),
     });
@@ -21,9 +28,28 @@ export default function TestRenderPrompt() {
     console.log(result);
   }
 
-  useEffect(() => {
-    getPrompt();
-  }, []);
+  // useEffect(() => {
+  //   getPrompt();
+  // }, []);
 
-  return <div>{responseContent}</div>;
+  return (
+    <div>
+      {/* <form
+        className="p-5"
+        onSubmit={(e) => {
+          e.preventDefault();
+          getPrompt();
+        }}
+      >
+        <input
+          className="border "
+          onChange={(e) => setUserPrompt(e.target.value)}
+        ></input>
+        <button className="border p-5" type="submit">
+          submitPrompt
+        </button>
+      </form> */}
+      <div className="p-5">{responseContent}</div>
+    </div>
+  );
 }
--- src/components/TestRenderSearch.tsx
+++ src/components/TestRenderSearch.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface resultProps {
   title: string;
@@ -10,19 +10,58 @@ interface resultProps {
 export default function TestRenderSearch() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResult, setSearchResult] = useState<resultProps[]>([]);
+  const [promptInject, setPromptInject] = useState("");
+  const [promptAnswer, setPromptAnswer] = useState("");
+  const [isPromptReady, setIsPromptReady] = useState(false);
 
   async function getResult() {
-    let request = await fetch("/api/get-search", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ searchQuery: searchQuery }),
-    });
-    const response = await request.json();
-    setSearchResult(response.searchResponse.web.results);
-    console.log(response);
+    try {
+      let request = await fetch("/api/get-search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ searchQuery: searchQuery }),
+      });
+      const response = await request.json();
+      const jsonResponseString = JSON.stringify(
+        response.searchResponse.web.results
+      );
+      setPromptInject(jsonResponseString);
+      setSearchResult(response.searchResponse.web.results);
+      setIsPromptReady(true);
+    } catch (e) {
+      console.error(e);
+    }
   }
+
+  async function getPromptAnswer() {
+    try {
+      let response = await fetch("http://localhost:11434/api/generate", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          model: "gemma3:1b",
+          prompt: `please summarise the following search results'${promptInject}'`,
+          stream: false,
+        }),
+      });
+      console.log(searchResult);
+      console.log(promptInject, "this is console log");
+
+      let result = await response.json();
+
+      setPromptAnswer(result.response);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    if (isPromptReady) {
+      getPromptAnswer();
+    }
+  }, [isPromptReady]);
 
   return (
     <div>
@@ -40,11 +79,12 @@ export default function TestRenderSearch() {
           Submit
         </button>
       </form>
-      <div>
+      {/* <div>
         {searchResult.map((item) => (
           <p key={item.url}>{item.title}</p>
         ))}
-      </div>
+      </div> */}
+      <div>{promptAnswer}</div>
     </div>
   );
 }
